f94051b2542f4da0680a5d5e54b9fe1962aadd30
Yeffercito <yeffermlopez@gmail.com>
2023-02-27 12:15:23 -0500

    Hola wapo

--- backyeff.js
+++ backyeff.js
@@ -1,3 +1,26 @@
-const nombre = document.getElementById("nombre");
-const edad = document.getElementById("edad");
-const preg1 = document.getElementById("preg1");
+const nombre1 = document.getElementById("nombre1");
+const saludo = document.getElementById("saludo");
+const legal = document.getElementById("legal");
+
+nombre1.addEventListener("click", function () { 
+    const nombre = document.getElementById("nombre").value;
+  console.log(`Su nombre es ${nombre}`);
+});
+
+saludo.addEventListener("click", function () {
+  console.log("Holaaaa");
+});
+
+legal.addEventListener("click", function () { 
+    const edad = document.getElementById("edad").value;
+ if(isNaN(edad)){
+    console.log("Ingrese un numero"); 
+}else{
+
+    if (edad >= 18) {
+        console.log("Es mayor de edad");
+      } else {
+        console.log("Es menor de edad");
+      }
+}
+});
